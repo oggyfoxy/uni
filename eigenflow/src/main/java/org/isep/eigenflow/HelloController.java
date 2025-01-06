62475c9b7b7bb337b4defab21871c13960cbda01
Alfa345 <legrandjacques.ruben@gmail.com>
2025-01-06 14:33:05 +0100

    Added Scene Builder parameters

--- eigenflow/src/main/java/org/isep/eigenflow/HelloController.java
+++ eigenflow/src/main/java/org/isep/eigenflow/HelloController.java
@@ -1,22 +1,107 @@
 package org.isep.eigenflow;
 
+import javafx.fxml.FXML;
+
 public class HelloController {
-   /*@FXML
-    private ListView<String> todoList;
-    @FXML
-    private ListView<String> inProgressList;
-    @FXML
-    private ListView<String> doneList;
-
-    @FXML
-
     
+    @FXML
     private void handleNewTask() {
-        todoList.getItems().add("New Task");
-        System.out.println("You selected a new item");
+        System.out.println("New task added.");
     }
-    */
-    public void UIPersonel(){
-        System.out.println("Opening Personel...");
+
+    @FXML
+    private void handleOpen() {
+        System.out.println("Open selected.");
+    }
+
+    @FXML
+    private void handleClose() {
+        System.out.println("Close selected.");
+    }
+
+    @FXML
+    private void handleSave() {
+        System.out.println("Save selected.");
+    }
+
+    @FXML
+    private void handleQuit() {
+        System.out.println("Quit application.");
+    }
+
+    @FXML
+    private void handleProfiles() {
+        System.out.println("Opening Profiles...");
+    }
+
+    @FXML
+    private void handleTeamManagement() {
+        System.out.println("Opening Team Management...");
+    }
+
+    @FXML
+    private void handleHistoryAndRoles() {
+        System.out.println("Opening History and Roles...");
+    }
+
+    @FXML
+    private void handleKanbanView() {
+        System.out.println("Opening Kanban View...");
+    }
+
+    @FXML
+    private void handleCalendarView() {
+        System.out.println("Opening Calendar View...");
+    }
+
+    @FXML
+    private void handleProgressCharts() {
+        System.out.println("Opening Progress Charts...");
+    }
+
+
+    @FXML
+    private void handlePerformance() {
+        System.out.println("Opening Performance Report...");
+    }
+
+    @FXML
+    private void handleBudgetAnalysis() {
+        System.out.println("Opening Budget Analysis Report...");
+    }
+
+    @FXML
+    private void handleMyTasks() {
+        System.out.println("Opening My Tasks...");
+    }
+
+    @FXML
+    private void handleProjectTasks() {
+        System.out.println("Opening Project Tasks...");
+    }
+
+    @FXML
+    private void handleNewProject() {
+        System.out.println("Creating New Project...");
+    }
+
+    @FXML
+    private void handleActiveProjects() {
+        System.out.println("Opening Active Projects...");
+    }
+
+    @FXML
+    private void handleArchivedProjects() {
+        System.out.println("Opening Archived Projects...");
+    }
+
+    @FXML
+    private void handleSettings() {
+        System.out.println("Opening Settings...");
+    }
+
+    @FXML
+    private void handleHelp() {
+        System.out.println("Opening Help...");
     }
 }
