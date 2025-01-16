34ed1dd1952ebe4aad2f6fc8e62f160b1c8cb643
Alfa345 <legrandjacques.ruben@gmail.com>
2025-01-16 22:59:44 +0100

    new files

--- eigenflow/src/main/java/module-info.java
+++ eigenflow/src/main/java/module-info.java
@@ -6,9 +6,8 @@ module org.isep.eigenflow {
     requires org.slf4j.simple;
     requires javafx.graphics;
 
-    // this line is crucial - it lets javafx see your classes
     exports org.isep.eigenflow.ui;
-    // also need this for fxml to work with your controller
+    
     opens org.isep.eigenflow.ui to javafx.fxml;
     opens org.isep.eigenflow.domain to javafx.base;
 
